3be2162c0826723d0264ec46dab55fc009cfedec
PVigar88 <Pvigar@gmail.com>
2021-04-10 00:29:01 -0400

    created react app and component files

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TeamMember from "./components/TeamMember";
+import TeamForm from "./components/TeamForm";
+
+function App() {
+  return (
+    <div className="App">
+      <TeamMember />
+      <TeamForm />
+    </div>
+  );
+}
+
+export default App;
