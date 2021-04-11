0ffb64f8590a8a31c3428b2f663d18c2e8c2b2d6
PVigar88 <Pvigar@gmail.com>
2021-04-11 17:07:03 -0400

    MVP complete

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,53 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import TeamMember from "./components/TeamMember";
 import TeamForm from "./components/TeamForm";
+import axios from "./axios";
+
+const initialFormData = {
+  username: "",
+  email: "",
+  role: "",
+};
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
+
+  const updateForm = (inputKey, inputValue) => {
+    setFormData({ ...formData, [inputKey]: inputValue });
+  };
+  const submitForm = () => {
+    // 🔥 STEP 9 - IMPLEMENT a submit function which will be used inside the form's own `onSubmit`
+    //  a) make a new friend object, trimming whitespace from username and email
+    const newMember = {
+      name: formData.name.trim(),
+      email: formData.email.trim(),
+      role: formData.role,
+    };
+
+    if (!newMember.username && !newMember.email && !newMember.role) return;
+    axios
+      .post("fakeapi.com", newMember)
+      .then((res) => {
+        setTeamMembers([...teamMembers, newMember]);
+        setFormData(initialFormData);
+      })
+      .catch((err) => {
+        console.log("an error has occured");
+      });
+  };
+
+  useEffect(() => {
+    axios.get("fakeapi.com").then((res) => setTeamMembers(res.data));
+  }, []);
+
   return (
     <div className="App">
-      <TeamMember />
-      <TeamForm />
+      {teamMembers.map((member) => {
+        return <TeamMember key={member.id} info={member} />;
+      })}
+      <TeamForm data={formData} update={updateForm} submit={submitForm} />
     </div>
   );
 }
--- team-builder/src/components/TeamMember.js
+++ team-builder/src/components/TeamMember.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export default function TeamMember() {
+export default function TeamMember(props) {
+  const { info } = props;
+
+  if (!info) {
+    return <h4>Loading...</h4>;
+  }
+
   return (
-    <div>
-      <p>Team Member</p>
+    <div className="teamMember">
+      <h2>{info.name}</h2>
+      <p>Email: {info.email}</p>
+      <p>Role: {info.role}</p>
     </div>
   );
 }
